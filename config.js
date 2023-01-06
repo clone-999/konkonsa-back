847ff27437ddb80cea078d4257d8b906e664f3ff
clone-999 <dbako2000@gmail.com>
2023-01-06 19:28:02 +0400

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "realdeal",
+    EMAIL: "alexandria66@ethereal.email", // testing email & password
+    PASSWORD : "QnEv7Qcda4Rb4J5R1F",
+    ATLAS_URI: "mongodb+srv://komko_user:kikps990@cluster0.y0ysoat.mongodb.net/?retryWrites=true&w=majority"
+}
